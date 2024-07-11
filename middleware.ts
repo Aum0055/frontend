1fa2c1667e5620d8a631659b2e06f9e4c5601624
Aum0055 <aumnew1598@gmail.com>
2024-07-11 20:00:35 +0700

    middleware

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,9 @@ export function middleware(request: NextRequest) {
   const res = NextResponse.next();
   const path = request.nextUrl.pathname;
  
-  // if (path != "/auth/sign-in") {
-  //   return NextResponse.redirect(new URL("/auth/sign-in", request.url));
-  // }
+  if (path != "/auth/sign-in") {
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
+  }
 
   return res;
 }
